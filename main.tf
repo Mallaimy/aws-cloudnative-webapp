4ca76269fc80181bb35a0e1ac564138c59cfdc47
Abakar Mahamat Mallah <abakar0809@gmail.com>
2026-04-26 08:26:17 -0400

    Phase 1: Add 6 subnets across 2 AZs (public, private, db)

--- main.tf
+++ main.tf
@@ -16,3 +16,58 @@ resource "aws_internet_gateway" "main" {
     Name = "${var.project_name}-igw"
   }
 }
+
+data "aws_availability_zones" "az" {
+  state = "available" # this shows all the available AZs in the region
+
+}
+
+locals {
+  azs    = slice(data.aws_availability_zones.az.names, 0, 2)
+  az_map = { for idx, az in local.azs : az => idx }
+}
+
+# Public subnets — one per AZ
+resource "aws_subnet" "public" {
+  for_each = local.az_map
+
+  vpc_id                  = aws_vpc.main.id
+  availability_zone       = each.key
+  cidr_block              = cidrsubnet(var.vpc_cidr, 8, each.value)
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.project_name}-public-${each.key}"
+    Tier = "public"
+  }
+}
+
+# private subnets - one per AZ
+resource "aws_subnet" "private" {
+  for_each                = local.az_map
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = cidrsubnet(var.vpc_cidr, 8, each.value + 10)
+  availability_zone       = each.key
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "${var.project_name}-private-${each.key}"
+    Tier = "private"
+  }
+
+}
+
+# database subnet - one per AZ
+resource "aws_subnet" "db" {
+  for_each                = local.az_map
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = cidrsubnet(var.vpc_cidr, 8, each.value + 20)
+  availability_zone       = each.key
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "${var.project_name}-db-${each.key}"
+    Tier = "db"
+  }
+
+}
